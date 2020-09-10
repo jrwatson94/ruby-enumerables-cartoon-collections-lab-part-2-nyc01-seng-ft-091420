88cbef1df46f8fb86b029903b7f352d5dc7d3262
Reid Watson <j.watsonreid@gmail.com>
2020-09-10 21:33:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,8 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.any? do |element|
       valid_calls.each do |call|
-        element == call
+        if element == call
+          
       end
   end
 end
